968c7030260a82f89d76ba172bc70cd7f6016f98
Ch3LL <mwilhite@saltstack.com>
2016-09-14 13:59:01 -0600

    fix docker container

--- docker_salt.zsh
+++ docker_salt.zsh
@@ -1,11 +1,11 @@
 LOCAL_VOLUME="${HOME}/devel/salt/"
 
 csalt_func() {
-    sudo /usr/bin/docker run --rm -itv $LOCAL_VOLUME:/testing $1-salt /bin/bash
+    sudo /usr/bin/docker run --rm -itv $LOCAL_VOLUME:/testing salt-$1 /bin/bash
     }
 
 ctest_func() {
-    sudo /usr/bin/docker run --rm -itv $LOCAL_VOLUME:/testing $1-salt python2 /testing/tests/runtests.py -n $2
+    sudo /usr/bin/docker run --rm -itv $LOCAL_VOLUME:/testing salt-$1 python2 /testing/tests/runtests.py -n $2
 }
 
 alias cshell='csalt_func'
